28c6ba28b54057d741a2bbad6dec370debebd253
Roman <kotubeev@mail.ru>
2020-06-25 19:45:24 +0700

    changed databases

--- medservice/settings.py
+++ medservice/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'medservice.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+   'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   'NAME': os.environ.get('DATABASENAME', ''),
+   'USER': os.environ.get('DATABASEUSER', ''),
+   'PASSWORD': os.environ.get('DATABASEPASSWORD', ''),
+   'HOST': os.environ.get('DATABASEHOST', ''),
+   'PORT': '5432',
+  }
 }
 
 
